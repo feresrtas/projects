ddfb96593d48afc16938cc026a12e48b2901ba7c
feresrtas <feresrtas.gmail.com>
2022-09-25 20:25:09 +0300

    xyz1

--- src/blog/models.py
+++ src/blog/models.py
@@ -17,6 +17,6 @@ class Post(models.Model):
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     publish_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField (auto_now=True)
-    author = models.ForeignKey(User.on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.Charfield (max_length=10, choices=OPTIONS, default="D")
     slug = models.SlugField(blank=True, unique=True)
